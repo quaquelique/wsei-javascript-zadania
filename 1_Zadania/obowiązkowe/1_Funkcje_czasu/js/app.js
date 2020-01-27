cc01296347ef7a5e7523ea25f0ddb8d9ae768a57
Karol Makowski <makowskikarol@outlook.com>
2020-01-27 15:52:17 +0100

    1. Funkcje czasu

---
+++ 1_Zadania/obowiązkowe/1_Funkcje_czasu/js/app.js
@@ -0,0 +1,15 @@
+function countHello(range)
+{
+    let interval = setInterval(write, 1000);
+
+    let count = 1;
+
+    function write()
+    {
+        if (count >= range) clearInterval(interval);
+
+        console.log("Hello " + count++);
+    }
+}
+
+countHello(10);
