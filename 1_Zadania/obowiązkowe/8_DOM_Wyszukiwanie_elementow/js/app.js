54f92e0c03984e18bbe8bb584433f9bdd32de3e5
Karol Makowski <makowskikarol@outlook.com>
2020-01-27 22:13:42 +0100

    done

--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -1,6 +1,35 @@
-document.addEventListener("DOMContentLoaded", function () {
-    /*
-    Poniżej napisz kod rozwiązujący zadania. Odpowiedzi oddzielaj komentarzami.
-     */
+// 8. DOM - getting elements of the HTML page.
 
+document.addEventListener("DOMContentLoaded", function ()
+{
+    // Zadanie 0
+    const title = document.getElementsByClassName('title')[0];
+
+    function getDataAnimation(element)
+    {
+        return element.getAttribute('data-animation')
+    }
+    console.log(getDataAnimation(title));
+
+    // Zadanie 1
+    const home = document.getElementById('home');
+    // const home = document.querySelector('#home');
+    const li = document.querySelector('[data-direction]');
+    const block = document.querySelector('.block');
+
+    console.log({ home, li, block });
+
+    // Zadanie 2
+    const lisInNavs = document.querySelectorAll('nav li');
+    const psInDivs = document.querySelectorAll('div p');
+    const divsInArticles = document.querySelectorAll('article div');
+
+    console.log({ lisInNavs, psInDivs, divsInArticles });
+
+    // Zadanie 3
+    const article = document.querySelector('article.first');
+    const h1 = article.querySelectorAll('h1');
+
+    console.log(h1);
+    console.log(h1.length);
 });
