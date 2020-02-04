3714ff7b0fa707a7e2446a6a12f1615b4714be5f
Karol Makowski <makowskikarol@outlook.com>
2020-02-04 20:46:01 +0100

    done

--- 1_Zadania/dodatkowe/11_Array_operations/js/app.js
+++ 1_Zadania/dodatkowe/11_Array_operations/js/app.js
@@ -1,7 +1,45 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var numbers = [5, 10, 12, 20, 100];
-    var result = numbers.reduce(function (prev, curr, index, array) {
-        return prev * curr;
-    })
-    console.log(result);
-})
+document.addEventListener('DOMContentLoaded', () =>
+{
+    // #0
+    {
+        var lengths = array => array.map(x => x.length);
+
+        console.log('\n*** Task 0 ***\n');
+        console.log(lengths(new Array('Kraków', 'Olsztyn', 'Szczecin', 'Ostrów Wielkopolski')));
+    }
+    // #1
+    {
+        var randomize = (param1, param2, callback) =>
+        {
+            var random = Math.random() * Math.abs(param1 - param2) + Math.min(param1, param2);
+            if (typeof callback !== 'function') return;
+            callback(random.toFixed(2));
+        };
+
+        console.log('\n*** Task 1 ***\n');
+        randomize(10, 20, console.log)
+    }
+    // #2
+    {
+        var write = array => array.forEach(x => console.log(x));
+
+        console.log('\n*** Task 2 ***\n');
+        write(new Array('cat', 'shrimp', 'giraffe'));
+    }
+    // #3
+    {
+        var ages = array => array.map(x => new Date().getFullYear() - x);
+
+        console.log('\n*** Task 3 ***\n');
+        console.log(ages(new Array(1995, 1856, 2014, 1987)));
+    }
+    // #4
+    {
+        var aggregate = array => array.reduce((x, y) => x + y);
+        var product = array => array.reduce((x, y) => x * y);
+
+        console.log('\n*** Task 4 ***\n');
+        console.log(aggregate(new Array(1, 2, 3, 4)));
+        console.log(product(new Array(1, 2, 3, 4)));
+    }
+});
