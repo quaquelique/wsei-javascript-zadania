5b7a9c570a6373f5b7da70221cdf23443db40092
Karol Makowski <makowskikarol@outlook.com>
2020-02-04 23:20:46 +0100

    done

--- 1_Zadania/dodatkowe/13_Arrow_functions/js/zadanie07.js
+++ 1_Zadania/dodatkowe/13_Arrow_functions/js/zadanie07.js
@@ -1,14 +1,26 @@
-const App = function () {
-  this.websites = ['onet', 'wp', 'facebook'];
-  this.links = [];
-}
+const App = function ()
+{
+    this.websites = ['onet', 'wp', 'facebook'];
+    this.links = [];
+};
 
 
-App.prototype.generateLinks = function () {
+App.prototype.generateLinks = function ()
+{
+    for (var website of this.websites)
+    {
+        this.links.push(`https://${website}.pl`);
+    }
+};
 
-}
-
+jQuery(document).ready(() =>
+{
+    $('a').each(function(index)
+    {
+        $(this).attr('href', app.links[index]);
+    });
+});
 
 let app = new App();
 app.generateLinks();
-console.log(app.links)
+console.log(app.links);
