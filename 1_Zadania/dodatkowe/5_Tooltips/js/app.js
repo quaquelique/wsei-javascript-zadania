82abb56f25042a6bbdb209c5c2bc67512751605c
Karol Makowski <makowskikarol@outlook.com>
2020-02-04 13:19:50 +0100

    done

--- 1_Zadania/dodatkowe/5_Tooltips/js/app.js
+++ 1_Zadania/dodatkowe/5_Tooltips/js/app.js
@@ -1,3 +1,22 @@
 /*
  <span class="tooltipText">Text tooltipa</span>
  */
+document.addEventListener('DOMContentLoaded', () =>
+{
+    var tooltips = document.querySelectorAll('.tooltip');
+    for (var tooltip of tooltips)
+    {
+        tooltip.addEventListener('mouseover', function ()
+        {
+            var text = document.createElement('span');
+            text.classList.add('tooltipText');
+            text.innerHTML = this.dataset.text;
+            this.appendChild(text);
+        });
+        tooltip.addEventListener('mouseout', function ()
+        {
+            var text = this.querySelector('.tooltipText');
+            if (text !== null) this.removeChild(text);
+        });
+    }
+});
