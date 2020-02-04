21d3f028deb76f91e3a2477e32640e0813cf2ea1
Karol Makowski <makowskikarol@outlook.com>
2020-02-04 02:40:58 +0100

    done

--- 1_Zadania/dodatkowe/2_Gallery/js/app.js
+++ 1_Zadania/dodatkowe/2_Gallery/js/app.js
@@ -4,3 +4,32 @@
    <button class="close">Close</button>
  </div>
  */
+document.addEventListener('DOMContentLoaded', () =>
+{
+    var elements = document.querySelectorAll('li');
+    var body = document.querySelector('body');
+
+    for (var image of document.querySelectorAll('img'))
+    {
+        image.addEventListener('click', function ()
+        {
+            var div = document.createElement('div');
+            div.classList.add('fullScreen');
+
+            var img = document.createElement('img');
+            img.src = this.src;
+            div.appendChild(img);
+
+            var button = document.createElement('button');
+            button.classList.add('close');
+            button.innerHTML = 'Close';
+            div.appendChild(button);
+            button.addEventListener('click', () =>
+            {
+                body.removeChild(div);
+            });
+
+            body.appendChild(div);
+        });
+    }
+});
