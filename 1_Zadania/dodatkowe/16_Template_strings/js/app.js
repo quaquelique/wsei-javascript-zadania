e18043af9f41bb3a28fd8fd8f93e9392e3d28e18
Karol Makowski <makowskikarol@outlook.com>
2020-02-05 01:36:00 +0100

    done

--- 1_Zadania/dodatkowe/16_Template_strings/js/app.js
+++ 1_Zadania/dodatkowe/16_Template_strings/js/app.js
@@ -1,17 +1,40 @@
-document.addEventListener('DOMContentLoaded', function () {
+// #0
+var person =
+    {
+        name: 'Vlad',
+        lastName: 'Drăculea',
+        yearOfBirth: 1431,
+        profession: 'Lord of Wallachia'
+    };
 
-    const array = [2, 15, 268, 288, [358, 2, 14, 256], 586];
-    // var sorted= array.sort((a,b)=>a-b);
-    // // console.log(sorted);
-    // // var newVar = array.flat();
-    // // console.log(newVar);
-    // newVar.sort((a,b)=>a-b);
-    var flat = array.flat();
-    flat.sort((a, b) => a - b);
-    console.log(flat);
-    var newFlat = flat.map((element) => element * 2);
-    console.log(newFlat);
-    var newVar = newFlat.reduce((a, b) => a * b);
-    console.log(newVar);
+var whoAreYou = person => `My name is ${person.name} ${person.lastName}.\n` +
+                `I am ${new Date().getFullYear() - person.yearOfBirth} years old.\n` +
+                `My profession is ${person.profession}\n`;
+console.log(whoAreYou(person));
 
-})
+// #1
+var first = 2;
+var second = 4;
+console.log(`Suma dwóch liczb ${first} i ${second} to: ${first + second}\n`);
+
+// #2
+var name = 'Karol';
+console.log(`Moje imię i nazwisko to ${name} Makowski\n`);
+
+// #3
+console.log('Myślę, że jest wiele piękna\n' +
+    'w posiadaniu problemów.\n' +
+    'To jeden ze sposobów w jaki się uczymy\n' +
+    'Herbie Hancock\n');
+
+// #4
+var button =
+    {
+        value: 'Send message',
+        idName: 'sendMsg',
+        width: '100px',
+        padding: '20px'
+    };
+console.log('To jest button.\n' +
+    `Jego szerokość to ${Number.parseInt(button.width.replace('px', '')) + 40}px\n` +
+    `Napis, który na nim widnieje to: "${button.value}"`);
